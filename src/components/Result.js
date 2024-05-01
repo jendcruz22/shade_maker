52240550c1f76742ca8987019d6eff0e6474697c
Jenny Dcruz <47320326+jendcruz22@users.noreply.github.com>
2024-05-01 13:16:08 -0400

    modified: result component display values

--- src/components/Result.js
+++ src/components/Result.js
@@ -4,15 +4,16 @@ import { Row, Col, Card } from "react-bootstrap";
 function Result({ colors }) {
   // Helper function to create RGB color string
   const rgbColor = (r, g, b) => `rgb(${r},${g},${b})`;
+  console.log(colors);
 
   return (
     <Card style={{ width: 'auto', marginTop: '15px' }}>
       <div style={{ backgroundColor: colors.hex.value, height: '10px' }}></div>
       <Card.Body>
-        <Card.Title>{colors.name.value}</Card.Title>
-        <Card.Text>
-          {colors.hex.value}
-        </Card.Text>
+        {/* Color Name + Hex Value */}
+        <Card.Title>{colors.name.value} ({colors.hex.value})</Card.Title>
+
+        {/* Color Blocks */}
         <Row>
           <Col>
             <div style={{ backgroundColor: rgbColor(colors.rgb.r, 0, 0), display: 'inline-block', margin: '0px 2px', height: '20px', width: '100%' }}></div>
@@ -24,15 +25,43 @@ function Result({ colors }) {
             <div style={{ backgroundColor: rgbColor(0, 0, colors.rgb.b), display: 'inline-block', margin: '0px 2px', height: '20px', width: '100%' }}></div>
           </Col>
         </Row>
+
+        {/* RGB Values */}
         <Row>
           <Col className="text-center">
-            R: {colors.rgb.r}
+            <b>R:</b>{colors.rgb.r}
           </Col>
           <Col className="text-center">
-            G: {colors.rgb.g}
+            <b>G:</b>{colors.rgb.g}
           </Col>
           <Col className="text-center">
-            B: {colors.rgb.b}
+            <b>B:</b>{colors.rgb.b}
+          </Col>
+        </Row>
+
+        {/* RGB Percentages */}
+        <Row>
+          <Col className="text-center">
+            <b>R%:</b>{(colors.rgb.fraction.r * 100).toFixed(2)}
+          </Col>
+          <Col className="text-center">
+            <b>G%:</b> {(colors.rgb.fraction.g * 100).toFixed(2)}
+          </Col>
+          <Col className="text-center">
+            <b>B%:</b> {(colors.rgb.fraction.b * 100).toFixed(2)}
+          </Col>
+        </Row>
+
+        {/* hsv Percentages */}
+        <Row>
+          <Col className="text-center">
+            <b>H:</b>{colors.hsv.h}
+          </Col>
+          <Col className="text-center">
+            <b>S:</b>{colors.hsv.s}
+          </Col>
+          <Col className="text-center">
+            <b>V:</b>{colors.hsv.v}
           </Col>
         </Row>
       </Card.Body>
